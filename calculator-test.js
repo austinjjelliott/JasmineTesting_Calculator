aac0d156083563ce3ac4e62a47ef7e00a0d575cf
Austin E <austinjjelliott@gmail.com>
2024-06-13 12:00:11 -0700

    ran 3 tests

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,30 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...typing random shit here 
+  const values = {
+    amount: 100,
+    years: 10,
+    rate: 5
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('1.06')
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..test test test 
+  const values = {
+    amount: 50000,
+    years: 25,
+    rate: 9
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('419.60')
 });
+
+it("should work when given decimal values",() => {
+  const values = {
+    amount: 2500.75,
+    years: 10.5,
+    rate: 4.66
+  }
+  expect(calculateMonthlyPayment(values)).toEqual('25.14');
+  });
 
 /// etc (test)
