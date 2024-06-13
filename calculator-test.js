e5435356f3fff911d289df33748ca2a142013aa1
Austin E <austinjjelliott@gmail.com>
2024-06-13 11:41:38 -0700

    testing commit

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,11 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  // ...typing random shit here 
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  // ..test test test 
 });
 
-/// etc
+/// etc (test)
